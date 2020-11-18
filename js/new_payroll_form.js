bed7522aa58470c4aa5436d4dc3da2a4d943417f
lkvreddy007 <lkvreddy007@gmail.com>
2020-11-19 02:22:15 +0530

    [Krishna]added-abilityToCreateEmployeePayrollObjectOnSave

--- js/new_payroll_form.js
+++ js/new_payroll_form.js
@@ -16,41 +16,50 @@ text.addEventListener('input', function () {
 });
 
 function save(){
-    var name = document.getElementById("name").value;
-
-    const pics = document.getElementsByName("profile");
-    let profilePic = pics[0];
-    for(let i=0;i<pics.length;i++){
-        if(pics[i].checked){
-            profilePic = pics[i].value;
-        }
-    }
-
-    let genders = document.getElementsByName("gender");
-    for(let i=0;i<genders.length;i++){
-        if(genders[i].checked)
-            gender=genders[i].value;
-    }
-
-    let departments = new Array();
-    const departmentsForm = document.getElementsByClassName("checkbox");
-    for(let i=0;i<departmentsForm.length;i++){
-        if(departmentsForm[i].checked)
-            departments.push(departmentsForm[i].value);
-    }
-
-    var salary = document.getElementById("salary").value;
-    var day = document.getElementById("day").value;
-    var month = document.getElementById("month").value;
-    var year = document.getElementById("year").value;
-    var startDate = new Date(parseInt(year), parseInt(month) - 1, parseInt(day));
-    var note = document.getElementById("notes").value;
     try{
-    const employeePayrollData = new EmployeePayrollData(name, profilePic, gender, departments, salary, startDate, note);
-    alert("Submitted"+employeePayrollData.toString());
+        let employeePayrollData = createEmployeePayrollData();
     }
     catch(e){
-        console.error(e);
+        return;
     }
+}
+
+const createEmployeePayrollData = () => {
+    let employeePayrollData = new EmployeePayrollData();
+    try{
+        employeePayrollData.notes = getInputValueById('#name');
+    }
+    catch(e){
+        setTextValue('.text-error',e);
+        throw e;
+    }
+    employeePayrollData.profilePic = getSelectedValues('[name=profile]').pop();
+    employeePayrollData.gender = getSelectedValues('[name=gender]').pop();
+    employeePayrollData.department = getSelectedValues('[name=department]');
+    employeePayrollData.salary = getInputValueById('#salary');
+    employeePayrollData.notes = getInputValueById('#notes');
+    let date = getInputValueById('#day')+" "+getInputValueById('#month')+" "+getInputValueById('#year');
+    employeePayrollData.date = Date.parse(date);
+    alert(employeePayrollData.toString());
+    return employeePayrollData;
+}
+
+const getSelectedValues = (propertValue) => {
+    let allItems = document.querySelectorAll(propertValue);
+    let selItems = [];
+    allItems.forEach(item=>{
+        if(item.checked) selItems.push(item.value);
+    });
+    return selItems;
+}
+
+const getInputValueById = (id) =>{
+    let value = document.querySelector(id).value;
+    return value;
+}
+
+const getInputElementValue = (id) =>{
+    let value = document.getElementById(id).value;
+    return value;
 }
 
